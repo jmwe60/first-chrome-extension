2a8b302aebb5c8c38df94253b43d77d4917a97f6
Juan Manuel Wong <42984309+jmwe60@users.noreply.github.com>
2025-06-03 15:42:09 -0600

    Update Content.js
    Added random image indexing

--- Content.js
+++ Content.js
@@ -6,3 +6,10 @@ let bobRossImages = [
 "https://bit.ly/3qCPjax",
 "https://bit.ly/3CkRXE6",
 ];
+
+const imgs = document.getElementsByTagName("img");
+
+for (image of imgs){
+  const index = Math.floor(Math.random()* bobRossImages.length);
+  image.src = bobRossImages[index];
+}
