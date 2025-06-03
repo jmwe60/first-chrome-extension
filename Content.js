67c1378cee0c862af822f578bd4f877452e07975
Juan Manuel Wong <42984309+jmwe60@users.noreply.github.com>
2025-06-03 15:38:37 -0600

    Create Content.js
    Link to images on bitly

---
+++ Content.js
@@ -0,0 +1,8 @@
+let bobRossImages = [
+"https://bit.ly/3Ck6DTU",
+"https://bit.ly/3ozQCVk",
+"https://bit.ly/3omYDN6",
+"https://bit.ly/3osrfoi",
+"https://bit.ly/3qCPjax",
+"https://bit.ly/3CkRXE6",
+];
